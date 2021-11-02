1c8fda8a25a6e27f4c6cb9e6d20711cc0d6da426
Xila Luna <xilaluna2@gmail.com>
2021-11-01 20:15:18 -0700

    [update] reformat to eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,9 @@ module.exports = {
     browser: true,
     es2020: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 12,
   },
-  rules: {
-  },
+  rules: {},
 };
